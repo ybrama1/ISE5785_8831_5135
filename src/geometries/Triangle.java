add9cd5700b1f500ab7d74361d78e10e1b8bf5c4
ybrama1 <baramay@yatmal.co.il>
2025-03-28 17:07:06 +0200

    added unitests for triangle and plane. the result should be true.

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -1,4 +1,6 @@
 package geometries;
+import primitives.Point;
+import primitives.Vector;
 
 /**
  * Class Triangle is the class representing a 2-Dimensional triangle in a Cartesian
@@ -6,4 +8,15 @@ package geometries;
  * @author Jeshurun and Binyamin
  */
 public class Triangle extends Polygon{
+    /**
+     * constructor creates the triangle
+     * @param p1 the first point on the triangle
+     * @param p2 the second point on the triangle
+     * @param p3 the third point on the triangle
+     */
+    public Triangle(Point p1, Point p2, Point p3) {
+        super(p1, p2, p3);
+    }
+
+
 }
--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -13,7 +13,7 @@ class PlaneTest {
      */
     @Test
     void Plane() {
-        Plane p = new Plane(new Point (1,1,1), new Point(2,2,2), new Point(3,3,3));
+        Plane p = new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(3, 3, 3));
 
         Vector v1 = new Point(1, 1, 1).subtract(new Point(2, 2, 2));
         Vector v2 = new Point(1, 1, 1).subtract(new Point(3, 3, 3));
@@ -25,54 +25,19 @@ class PlaneTest {
         // =============== Boundary Values Tests ==================
         // TC11: The first point is the same as the second point
         // the result should be exception
-        try {
-            new Plane(new Point(1, 1, 1), new Point(1, 1, 1), new Point(3, 3, 3));
-            fail("ERROR: Plane() for the same points does not throw an exception");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("ERROR: Plane() for the same points throws wrong exception");
-        }
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(1, 1, 1), new Point(3, 3, 3)), "ERROR: Plane() for the same points does not throw an exception");
         // TC12: The first point is the same as the third point
         // the result should be exception
-        try {
-            new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(1, 1, 1));
-            fail("ERROR: Plane() for the same points does not throw an exception");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("ERROR: Plane() for the same points throws wrong exception");
-        }
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(1, 1, 1)), "ERROR: Plane() for the same points does not throw an exception");
         // TC13: The second point is the same as the third point
         // the result should be exception
-        try {
-            new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(2, 2, 2));
-            fail("ERROR: Plane() for the same points does not throw an exception");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("ERROR: Plane() for the same points throws wrong exception");
-        }
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(2, 2, 2)), "ERROR: Plane() for the same points does not throw an exception");
         // TC14: The points are identical
         // the result should be exception
-        try {
-            new Plane(new Point(1, 1, 1), new Point(1, 1, 1), new Point(1, 1, 1));
-            fail("ERROR: Plane() for the same points does not throw an exception");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("ERROR: Plane() for the same points throws wrong exception");
-        }
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(1, 1, 1), new Point(1, 1, 1)), "ERROR: Plane() for the same points does not throw an exception");
         // TC15: The points are on the same line
         // the result should be exception
-        try {
-            new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(3, 3, 3));
-            fail("ERROR: Plane() for the same points does not throw an exception");
-        } catch (IllegalArgumentException e) {
-            assertTrue(true);
-        } catch (Exception e) {
-            fail("ERROR: Plane() for the same points throws wrong exception");
-        }
+        assertThrows(IllegalArgumentException.class, () -> new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(3, 3, 3)), "ERROR: Plane() for the same points does not throw an exception");
     }
 
 
@@ -82,14 +47,18 @@ class PlaneTest {
      */
     @Test
     void testGetNormal() {
-        Plane p = new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(3, 3, 3));
+        Plane p = new Plane(new Point(1, 1, 1), new Point(2, 2, 2), new Point(1, 2, 3));
 
         Vector v1 = new Point(1, 1, 1).subtract(new Point(2, 2, 2));
-        Vector v2 = new Point(1, 1, 1).subtract(new Point(3, 3, 3));
+        Vector v2 = new Point(1, 1, 1).subtract(new Point(1, 2, 3));
         // ============ Equivalence Partitions Tests ==============
         // TC01: Test for a normal orthogonal to 2 vectors in the plane
         // the result should be true
         assertTrue(p.getNormal().dotProduct(v1) == 0 && p.getNormal().dotProduct(v2) == 0, "ERROR: getNormal() wrong value");
+
+        //TC02: Test if the normal length is 1
+        // the result should be 1
+        assertEquals(1, p.getNormal().length(), "ERROR: getNormal() wrong value");
     }
 
     /**
@@ -106,6 +75,10 @@ class PlaneTest {
         // TC01: Test for a normal orthogonal to 2 vectors in the plane
         // the result should be true
         assertTrue(p.getNormal(new Point(1, 1, 1)).dotProduct(v1) == 0 && p.getNormal(new Point(1, 1, 1)).dotProduct(v2) == 0, "ERROR: getNormal(Point) wrong value");
+
+        //TC02: Test if the normal length is 1
+        // the result should be 1
+        assertEquals(1, p.getNormal(new Point(1, 1, 1)).length(), "ERROR: getNormal(Point) wrong value");
     }
 
 }
